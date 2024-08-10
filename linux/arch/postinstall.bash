f15353bc7cb6d094798edb5334b64d12d46ff6bd
suryansh <suryansh2048+git@gmail.com>
2024-08-10 23:57:03 +0530

    Changes to be committed:   modified:   linux/arch/postinstall.bash

--- linux/arch/postinstall.bash
+++ linux/arch/postinstall.bash
@@ -1,5 +1,5 @@
 pacman -Sy
-pacman -S neovim zsh networkmanager grub efibootmgr os-prober ntfs-3g amd-ucode --noconfirm
+pacman -S neovim zsh networkmanager ntfs-3g amd-ucode --noconfirm
 
 echo "en_IN UTF-8" > /etc/locale.gen
 locale-gen
@@ -28,9 +28,10 @@ echo "Defaults !tty_tickets" >> /etc/sudoers
 
 bootctl --path=/boot install
 
-reaad "grub or direct boot? (grub is better for dual boot) [g/d]: " boot
+read "grub or direct boot? (grub is better for dual boot) [g/d]: " boot
 
 if [ $boot == "g" ]; then
+    pacman -S grub efibootmgr os-prober --noconfirm
     grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
     grub-mkconfig -o /boot/grub/grub.cfg
 else
@@ -47,4 +48,4 @@ fi
 echo "KEYMAP=us" > /etc/vconsole.conf
 echo "arch" > /etc/hostname
 
-pacman -S plasma kitty sddm --noconfirm
+pacman -Sy plasma kitty sddm --noconfirm
